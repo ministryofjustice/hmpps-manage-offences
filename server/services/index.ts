8796e24e316bd47350469e582812be756148f85e
yasin <yasin.mustafa@gmail.com>
2023-12-01 12:44:52 +0000

    MOP-128 Re-introduce manage-users-api call to replace auth all (was temporarily removed whilst DPS team investigated)

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,17 +1,17 @@
 import OffenceService from './offenceService'
 import UserService from './userService'
-import HmppsAuthClient from '../data/hmppsAuthClient'
 import ManageOffencesApiClient from '../data/manageOffencesApiClient'
 import AdminService from './adminService'
 import PrisonApiClient from '../data/prisonApiClient'
+import ManageUsersApiClient from '../data/manageUsersApiClient'
 
-const hmppsAuthClient = new HmppsAuthClient()
 const manageOffencesApiClient = new ManageOffencesApiClient()
 const prisonApiClient = new PrisonApiClient()
+const manageUsersApiClient = new ManageUsersApiClient()
 
 const offenceService = new OffenceService(manageOffencesApiClient)
 const adminService = new AdminService(manageOffencesApiClient, prisonApiClient)
-const userService = new UserService(hmppsAuthClient)
+const userService = new UserService(manageUsersApiClient)
 
 export const services = {
   offenceService,
--- server/services/userService.test.ts
+++ server/services/userService.test.ts
@@ -1,28 +1,28 @@
 import UserService from './userService'
-import HmppsAuthClient, { AuthUserDetails } from '../data/hmppsAuthClient'
+import ManageUsersApiClient, { User } from '../data/manageUsersApiClient'
 
-jest.mock('../data/hmppsAuthClient')
+jest.mock('../data/manageUsersApiClient')
 
 const user = { token: 'some token' } as Express.User
 
 describe('User service', () => {
-  let hmppsAuthClient: jest.Mocked<HmppsAuthClient>
+  let manageUsersApiClient: jest.Mocked<ManageUsersApiClient>
   let userService: UserService
 
   describe('getUser', () => {
     beforeEach(() => {
-      hmppsAuthClient = new HmppsAuthClient() as jest.Mocked<HmppsAuthClient>
-      userService = new UserService(hmppsAuthClient)
+      manageUsersApiClient = new ManageUsersApiClient() as jest.Mocked<ManageUsersApiClient>
+      userService = new UserService(manageUsersApiClient)
     })
     it('Retrieves and formats user name', async () => {
-      hmppsAuthClient.getUser.mockResolvedValue({ name: 'john smith' } as AuthUserDetails)
+      manageUsersApiClient.getUser.mockResolvedValue({ name: 'john smith' } as User)
 
       const result = await userService.getUser(user)
 
       expect(result.displayName).toEqual('John Smith')
     })
     it('Propagates error', async () => {
-      hmppsAuthClient.getUser.mockRejectedValue(new Error('some error'))
+      manageUsersApiClient.getUser.mockRejectedValue(new Error('some error'))
 
       await expect(userService.getUser(user)).rejects.toEqual(new Error('some error'))
     })
