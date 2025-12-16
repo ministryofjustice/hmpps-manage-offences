de1a8e3f47d52318a810df659f954aea0a8c30c0
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2025-12-16 08:52:44 +0000

    Align with package json

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,10 +3,10 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 /* eslint-disable prettier/prettier */
 export default configureAllowedScripts({
    allowlist: {
-      'node_modules/@parcel/watcher@2.5.0': 'FORBID',
-      'node_modules/cypress@14.4.0': 'ALLOW',
+      'node_modules/@parcel/watcher@2.5.1': 'FORBID',
+      'node_modules/cypress@14.5.4': 'ALLOW',
       'node_modules/dtrace-provider@0.8.8': 'FORBID',
       'node_modules/fsevents@2.3.3': 'FORBID',
-      'node_modules/unrs-resolver@1.7.2': 'FORBID',
+      'node_modules/unrs-resolver@1.11.1': 'FORBID',
    },
 })
