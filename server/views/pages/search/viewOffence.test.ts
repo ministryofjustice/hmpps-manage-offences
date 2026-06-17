6e416bd7d87a2e7e6df982763f107123357d08fd
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2026-06-17 17:03:25 +0100

    MOP-235: Update test assertions to reflect "SDS40 Release" terminology

--- server/views/pages/search/viewOffence.test.ts
+++ server/views/pages/search/viewOffence.test.ts
@@ -44,7 +44,7 @@ describe('VIEW_OFFENCE /', () => {
 
     expect(offenceMarkers.length).toBe(1)
     expect(offenceMarkers.find('li').length).toBe(2)
-    expect(offenceMarkers.text()).toContain('SDS Early Release Exclusion: Sexual')
+    expect(offenceMarkers.text()).toContain('SDS40 Release Exclusion: Sexual')
     expect(offenceMarkers.text()).toContain('SDS+: SDS >7 years between 01 April 2020 and 28 June 2022')
     expect($('h1').text().trim()).toContain('AB: Offence 1')
   })
@@ -64,7 +64,7 @@ describe('VIEW_OFFENCE /', () => {
     expect(offenceMarkers.length).toBe(1)
     expect(offenceMarkers.find('li').length).toBe(1)
     expect(offenceMarkers.text()).toContain(
-      'SDS Early Release Exclusion: Violent (for sentences of four years and over)',
+      'SDS40 Release Exclusion: Violent (for sentences of four years and over)',
     )
   })
 
@@ -82,7 +82,7 @@ describe('VIEW_OFFENCE /', () => {
 
     expect(offenceMarkers.length).toBe(1)
     expect(offenceMarkers.find('li').length).toBe(1)
-    expect(offenceMarkers.text()).toContain('SDS Early Release Exclusion: Terrorism')
+    expect(offenceMarkers.text()).toContain('SDS40 Release Exclusion: Terrorism')
   })
 
   it('domestic abuse marker should be rendered correctly', () => {
@@ -99,7 +99,7 @@ describe('VIEW_OFFENCE /', () => {
 
     expect(offenceMarkers.length).toBe(1)
     expect(offenceMarkers.find('li').length).toBe(1)
-    expect(offenceMarkers.text()).toContain('SDS Early Release Exclusion: Domestic Abuse')
+    expect(offenceMarkers.text()).toContain('SDS40 Release Exclusion: Domestic Abuse')
   })
 
   it('domestic abuse marker should be rendered correctly', () => {
@@ -116,7 +116,7 @@ describe('VIEW_OFFENCE /', () => {
 
     expect(offenceMarkers.length).toBe(1)
     expect(offenceMarkers.find('li').length).toBe(1)
-    expect(offenceMarkers.text()).toContain('SDS Early Release Exclusion: National Security')
+    expect(offenceMarkers.text()).toContain('SDS40 Release Exclusion: National Security')
   })
 
   it('orphaned inchoate offences should display a warning banner', () => {
